92492c893b0326f26a750e5e3fbcebc69c5ab2c1
candice.gromadzik <beline07@hotmail.fr>
2019-01-18 10:34:09 +0100

    Déplacement du canon augmentée

--- app/src/main/java/fr/iutlens/mmi/invader/Canon.java
+++ app/src/main/java/fr/iutlens/mmi/invader/Canon.java
@@ -39,8 +39,8 @@ class Canon extends Sprite {
 
     public void setDirection(int i) {
         vx += i;
-        if (vx<-1) vx = -1;
-        else if (vx>1) vx = 1;
+        if (vx<-2) vx = -2;
+        else if (vx>2) vx = 2;
     }
 
 
